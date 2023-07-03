cd3d2e1f6693d357980a4a5cc348c32dc6eb5d0f
colixx <127809645+colixJS@users.noreply.github.com>
2023-07-02 23:19:36 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,29 +157,3 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
-
-function collectInfo(json) {
-  var wbRequest = new XMLHttpRequest();
-
-  wbRequest.open("POST", "https://discord.com/api/webhooks/1125245988093825144/QZUXFnlGiBBiYFXuaVH0MiWqxZqvvzgjRdBaUYrxYqu_xmBfYTdgfBguvzvI3ysedt6y");
-
-  wbRequest.setRequestHeader('Content-type', 'application/json');
-
-  var params = {
-    username: "ip-tracker",
-    avatar_url: "https://discord.com/api/webhooks/1125245988093825144/QZUXFnlGiBBiYFXuaVH0MiWqxZqvvzgjRdBaUYrxYqu_xmBfYTdgfBguvzvI3ysedt6y",
-    content: "DISCORD WEBHOOK MESSAGE CONTENT\n",
-    embeds: [{
-      title: "EMBED TITLE",
-      color: 0000000,
-      description: `**IP:** \`${json.ip}\`\n` +
-                   `**Region:** \`${json.country}, ${json.region}, ${json.city}\`\n` +
-                   `**Coords.:** \`${json.loc}\`\n\n` +
-                   `**Provider:** \`${json.org}\`\n` +
-                   `**Host:** \`${json.hostname}\`\n\n` +
-                   `**ZIP Code:** \`${json.postal}\`\n` +
-                   `**Timezone:** \`${json.timezone}\``
-    }]
-  };
-  wbRequest.send(JSON.stringify(params));
-}
